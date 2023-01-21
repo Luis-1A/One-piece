8c4f079e3b963cf3bed410202c5e14bd2b792be5
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-01-21 10:03:02 -0300

    content update and CSS file added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import './App.css';
 import Banner from './components/Banner';
+import Button from './components/Button';
 import Footer from './components/Footer';
 import Form from './components/Form';
 import Team from './components/Team';
@@ -45,6 +47,8 @@ function App() {
   ];
 
   const [members, setMembers] = useState([]);
+  const [showPirateForm, setShowPirateForm] = useState(false);
+  const [showCrewForm, setShowCrewForm] = useState(false);
 
   const onNewMemberAdded = (member) => {
     // console.log(member);
@@ -52,10 +56,42 @@ function App() {
     setMembers([...members, member]);
   }
 
+  // show/hide Registration forms
+  const toggleForm = (type) => {
+    if (type === 'pirate') {
+      setShowPirateForm(!showPirateForm);
+    }
+    if (type === 'crew') {
+      setShowCrewForm(!showCrewForm);
+    }
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Form teams={teams.map(team => team.name)} onRegisteredMember={member => onNewMemberAdded(member)} />
+
+      <main className='memorandum'>
+        <h1>The Worst Generation - Organization Charts</h1>
+        <strong>Attention all Officers and Infantry members stationed at the New World!</strong>
+        <p>The so-called "Worst Generation" is sailing our waters and it's imperative that we capture them and their crew.</p>
+        <p>Below you'll find the organizational chart of known pirate crews. If you find any of these rascals, do not engage in battle and report immediately to your Captain. I repeat: <strong>do not engage in battle!</strong> These are extremely dangerous pirates with bounties of over <img src='/images/belly.png' alt='belly currency' className='belly' />100.000.000.</p>
+        <p>If necessary, use the correspondent buttons to register a new pirate or pirate crew.</p>
+        <p className='signature'>— Fleet Admiral Sakazuki</p>
+      </main>
+
+      <div className="registration-buttons">
+        <Button onClick={() => toggleForm('pirate')}>+ New Pirate</Button>
+        <Button>+ New Pirate Crew</Button>
+      </div>
+
+      {/* {showPirateForm && <Form teams={teams.map(team => team.name)} onRegisteredMember={member => onNewMemberAdded(member)} />} */}
+
+      <Form 
+        teams={teams.map(team => team.name)} 
+        onRegisteredMember={member => onNewMemberAdded(member)}
+        active={showPirateForm} 
+      />
+
       {teams.map(team => (
         <Team 
           key={team.name} 
@@ -65,6 +101,7 @@ function App() {
           members={members.filter(member => member.team === team.name)}
         />
       ))}
+
       <Footer />
     </div>
   );
