e440a5cba81db2fe5a58168d69db3d92f0e0cadd
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-01-26 11:45:37 -0300

    content and CSS update

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,7 +3,7 @@ import './Banner.css';
 function Banner() {
     return (
         <header className="banner" >
-            <p>Pirate Crews Database</p>
+            <p>PIRATES DATABASE</p>
             <img src="/images/banner.png" alt="Marines flag top secret" />
         </header>
     )
