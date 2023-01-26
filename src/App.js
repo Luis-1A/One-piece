b5c521664de525a1f81eeebc2c9dd8a7dd48bd96
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-01-26 12:40:06 -0300

    "read more" button for mobile screens

--- src/App.js
+++ src/App.js
@@ -55,17 +55,25 @@ function App() {
     }
   }
 
+  const showMemorandum = (e) => {
+    document.querySelector('.memorandum').style.maxHeight = '100%';
+    e.target.parentNode.style.display = 'none';
+  }
+
   return (
     <div className="App">
       <Banner />
 
       <main className='memorandum'>
-        <h1>The Worst Generation - Organization Charts</h1>
-        <strong>Attention all Officers and Infantry members stationed at the New World!</strong>
+        <h1>Important Message</h1>
+        <p><strong>Attention all Officers and Infantry members stationed at the New World!</strong></p>
         <p>The so-called "Worst Generation" is sailing our waters and it's imperative that we capture them and their crew.</p>
         <p>Below you'll find the organizational chart of known pirate crews. If you find any of these rascals, do not engage in battle and report immediately to your Captain. I repeat: <strong>do not engage in battle!</strong> These are extremely dangerous pirates with bounties of over <img src='/images/belly.png' alt='belly currency' className='belly' />100.000.000.</p>
         <p>If necessary, use the correspondent buttons to register a new pirate or pirate crew.</p>
         <p className='signature'>— Fleet Admiral Sakazuki</p>
+        <div className="read-memorandum">
+          <button onClick={showMemorandum}>Read More</button>
+        </div>
       </main>
 
       <div className="registration-buttons">
