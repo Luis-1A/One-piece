29835c5deeb2d49cdf1650a2a96b30ad5579cd0e
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-01-31 16:52:52 -0300

    CSS update

--- src/components/Pirate/index.js
+++ src/components/Pirate/index.js
@@ -3,12 +3,11 @@ import './Pirate.css';
 const Pirate = ({ name, role, bounty, image, bg }) => {
     return (
         <div className='pirateContainer'>
-            <div className='header' style={{ backgroundColor: bg }}>
-                <img className='picture' src={image} alt={name} />
-            </div>
+            <div className='header' style={{ backgroundColor: bg }}></div>
+            <img className='picture' src={image} alt={name} />
             <div className='footer'>
                 <h4>{name}</h4>
-                <h5>{role}</h5>
+                <h5 className={role.toLowerCase() === 'captain' ? 'captain' : ''}>{role}</h5>
                 <h5><img className='belly' src="/images/belly.png" alt="belly currency icon" />{parseInt(bounty, 10).toLocaleString('en-US')}</h5>
             </div>
         </div>
