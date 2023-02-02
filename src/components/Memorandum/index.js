fa8fe0d7da0276a1b72870461b8f0a75c4380db6
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-02-02 16:07:33 -0300

    Memorandum component

---
+++ src/components/Memorandum/index.js
@@ -0,0 +1,23 @@
+import './Memorandum.css'
+
+// show full text (text is partially hidden on smaller screens)
+const showMemorandum = (e) => {
+    document.querySelector('.memorandum').style.maxHeight = '100%';
+    e.target.parentNode.style.display = 'none';
+}
+
+export default function Memoradum() {
+    return (
+        <main className='memorandum'>
+            <h1>Important Message</h1>
+            <p><strong>Attention all Officers and Infantry members stationed at the New World!</strong></p>
+            <p>The so-called "Worst Generation" is sailing our waters and it's imperative that we capture them and their crew.</p>
+            <p>Below you'll find the organizational chart of known pirate crews. If you find any of these rascals, do not engage in battle and report immediately to your Captain. I repeat: <strong>do not engage in battle!</strong> These are extremely dangerous pirates with bounties of over <img src='/images/belly.png' alt='belly currency' className='belly' />100.000.000.</p>
+            <p>If necessary, use the correspondent buttons to register a new pirate or pirate crew.</p>
+            <p className='signature'>— Fleet Admiral Sakazuki</p>
+            <div className="read-memorandum">
+                <button onClick={showMemorandum}>Read More</button>
+            </div>
+        </main>
+    )
+}
