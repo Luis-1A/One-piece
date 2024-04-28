3cc23fddb0e0ff6bd28aa1f65c945365d3cfc5a6
Luis Fernando santos <142061648+feito-pelo@users.noreply.github.com>
2024-04-27 23:09:17 -0300

    Update index.js

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,13 +1,12 @@
 import './Dropdown.css';
 
-const Dropdown = ({required, label, itens, option, onChange}) => {
-    
+const Dropdown = ({ required, label, items, option, onChange }) => {
     return (
         <div className='dropdown-container'>
             <label>{label}</label>
             <select required={required} value={option} onChange={(e) => onChange(e.target.value)}>
-                <option value="">Select a Crew</option>
-                {itens.map(item => <option key={item.id} value={item.id}>{item.name}</option>)}
+                <option value="">Selecione uma Tripulação</option>
+                {items.map(item => <option key={item.id} value={item.id}>{item.name}</option>)}
             </select>
         </div>
     );
